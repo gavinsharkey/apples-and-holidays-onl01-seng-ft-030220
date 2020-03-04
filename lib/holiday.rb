a63fba4dee31e81ffc1a310ae4dca1277ef6e6dc
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-04 00:11:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.upcase}:"
-    holiday.each do |holiday, supplies| 
+    holiday.each do |holiday, supplies|
       puts "#{holiday.to_s.upcase}: #{supplies.join(', ')}"
     end
   end
